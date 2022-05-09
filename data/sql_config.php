07b3c5b1cc3cabf6167eb36add7b6749a0fd65cd
votrieuvy1661 <votrieuvy1661@gmail.com>
2022-05-09 22:39:58 +0700

    new inject

--- data/sql_config.php
+++ data/sql_config.php
@@ -2,6 +2,6 @@
     $host       = "172.0.0.1:54504";
     $username   = "azure";
     $password   = "6#vWHD_$";
-    $dbname     = "localdb";
+    $dbname     = "vmusic";
     $charset = "utf8";
 ?>
