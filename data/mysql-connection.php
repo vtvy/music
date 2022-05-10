d08fae677bcd659519307346f87284fae77477c6
votrieuvy1661 <votrieuvy1661@gmail.com>
2022-05-11 01:29:57 +0700

    cert

--- data/mysql-connection.php
+++ data/mysql-connection.php
@@ -1,7 +1,6 @@
 <?php
     $conn = mysqli_init();
-    mysqli_ssl_set($conn,NULL,NULL, "./DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-    mysqli_real_connect($conn, 'mydemoserver.mysql.database.azure.com', 'test', 'test', 'vmusic', 3306, MYSQLI_CLIENT_SSL);
+    mysqli_real_connect($conn, 'mydemoserver.mysql.database.azure.com', 'test', 'test', 'vmusic', 3306);
     if (mysqli_connect_errno($conn)) {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
     }
